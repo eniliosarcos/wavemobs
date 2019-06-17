7f3ff8c1f7cd3c10243aab873a7577ae45a2e389
eniliosarcos <eniliosarcos1993@gmail.com>
2019-06-17 10:53:38 -0400

    player movement relative to camera forward

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float velocity = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        input = Camera.main.transform.TransformDirection(input);
+        input.y = 0.0f;
+        transform.position += input*Time.deltaTime*velocity;
+    }
+}
