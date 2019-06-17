d3875c278e1f8ca4e91d7c5dd178bfbc3d0cb739
eniliosarcos <eniliosarcos1993@gmail.com>
2019-06-17 16:21:58 -0400

    playermovement updated with rotation input

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float velocity = 3.0f;
+    public float speedMovement = 3.0f;
+    public float speedRotation = 4.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +15,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 targetdirection = new Vector3(Input.GetAxis("Horizontal right stick"), 0f, Input.GetAxis("Vertical right stick"));
+        if (targetdirection != Vector3.zero)
+        {
+            targetdirection = Camera.main.transform.TransformDirection(targetdirection);
+            targetdirection.y = 0.0f;
+            Quaternion targetrotation = Quaternion.LookRotation(targetdirection, Vector3.up * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetrotation, speedRotation * Time.fixedDeltaTime);
+        }
+
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         input = Camera.main.transform.TransformDirection(input);
         input.y = 0.0f;
-        transform.position += input*Time.deltaTime*velocity;
+        transform.position += input*Time.deltaTime*speedMovement;
     }
 }
