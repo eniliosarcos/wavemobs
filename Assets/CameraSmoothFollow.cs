eeec6416bea87edc3d698b89c94ef59c46e6382f
eniliosarcos <eniliosarcos1993@gmail.com>
2019-06-17 11:26:27 -0400

    smooth camera to follow player

---
+++ Assets/CameraSmoothFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmoothFollow : MonoBehaviour
+{
+    public Transform player;       //Public variable to store a reference to the player game object
+    public float smoothSpeed = 0.05f;
+    public Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        Vector3 _positionWithOffset = player.transform.position + offset;
+        Vector3 _smoothPosition = Vector3.Lerp(transform.position, _positionWithOffset, smoothSpeed);
+        transform.position = _smoothPosition;
+    }
+}
